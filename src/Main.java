24bb2528a7aa5949f53446a4d19472e61875742e
KevediBeb <kevin.torneus@elev.ga.ntig.se>
2023-10-09 14:58:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args) {
+
+        String balls = "balls";
+
+        String radie = JOptionPane.showInputDialog(null, "meter radie????? :OOOOOo (integer only)");
+
+        JOptionPane.showMessageDialog(null, (4 * Math.PI * Math.pow(3, Integer.parseInt(radie)))/3 + " metros de kubikos är din sfär");
+
+
+        System.out.println("AAAAAAAAAA");
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+    }
+}
